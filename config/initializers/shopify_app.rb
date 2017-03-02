d813f40ec8cde6da921ad6b6fe70630ba90932fd
Dustin Milam <darklordsushi@yahoo.com>
2017-03-02 11:10:24 -0500

    Added production uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,8 +1,10 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV["API_KEY"]
   config.secret = ENV["SECRET"]
-  if Rails.env.development?
-	  config.redirect_uri = "https://localhost:3000/auth/shopify/callback"
+  if Rails.env.production?
+	  config.redirect_uri = "https://easydash.herokuapp.com/auth/shopify/callback"
+	else
+		config.redirect_uri = "https://localhost:3000/auth/shopify/callback"
 	end
   config.scope = "read_content, write_content, read_themes, write_themes, read_products, write_products, read_script_tags, write_script_tags"
   config.embedded_app = true
