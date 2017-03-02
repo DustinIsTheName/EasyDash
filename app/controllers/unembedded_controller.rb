0beb2fc8cce7a3410aaada14822ece453cf4fa3b
Dustin Milam <darklordsushi@yahoo.com>
2017-03-02 09:18:46 -0500

    Made instance variables from Shopify's API for iframe

--- app/controllers/unembedded_controller.rb
+++ app/controllers/unembedded_controller.rb
@@ -4,6 +4,22 @@ class UnembeddedController < ApplicationController
   layout 'application'
 
   def quick_select
+
+  	@products = ShopifyAPI::Product.find(:all, params: {limit: 250})
+  	@articles = ShopifyAPI::Article.find(:all, params: {limit: 250})
+  	@pages = ShopifyAPI::Page.find(:all, params: {limit: 250})
+  	@collections = []
+
+		@cc = ShopifyAPI::CustomCollection.find(:all, params: {title: params[:q], :limit => 250})
+		@cc.each do |c|
+		  @collections << c
+		 end
+
+		@sc = ShopifyAPI::SmartCollection.find(:all, params: {title: params[:q], :limit => 250})
+		@sc.each do |c|
+		 @collections << c
+		end
+
   end
 
 end
