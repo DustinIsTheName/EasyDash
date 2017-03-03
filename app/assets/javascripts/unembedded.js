15f5c19b4c0224a251830cf03c96e51607af3d72
Dustin Milam <darklordsushi@yahoo.com>
2017-03-03 09:30:42 -0500

    finished initial dropdown structure

--- app/assets/javascripts/unembedded.js
+++ app/assets/javascripts/unembedded.js
@@ -1,1 +1,18 @@
-console.log('hello world!');
+$(document).ready(function() {
+
+	var blogs = $('.select-sim-dropdown.blog').data('object'),
+			collections = $('.select-sim-dropdown.collection').data('object'),
+			pages = $('.select-sim-dropdown.page').data('object'),
+			products = $('.select-sim-dropdown.product').data('object'),
+			blog_page = 1,
+			collection_page = 1
+			page_page = 1,
+			product_page = 1;
+
+
+	console.log(blogs);
+	console.log(collections);
+	console.log(pages);
+	console.log(products);
+
+});
