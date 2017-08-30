7a4976bd26dcb90c0e4b2fd53a1936bc982b88f2
Dustin Milam <darklordsushi@yahoo.com>
2017-08-30 08:55:26 -0400

    changed shopify configs to use the URL constants

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,9 +2,9 @@ ShopifyApp.configure do |config|
   config.api_key = ENV["API_KEY"]
   config.secret = ENV["SECRET"]
   if Rails.env.production?
-	  config.redirect_uri = "https://app.easydash.io/auth/shopify/callback"
+	  config.redirect_uri = "#{APP_URL}/auth/shopify/callback"
 	else
-		config.redirect_uri = "https://localhost:3000/auth/shopify/callback"
+		config.redirect_uri = "#{DEV_URL}/auth/shopify/callback"
 	end
   config.scope = "write_content, write_themes, write_products, write_script_tags, read_fulfillments"
   config.embedded_app = true
