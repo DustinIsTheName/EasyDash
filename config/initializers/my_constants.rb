19213f3673ad9128bbb7ff064dc0957c3ede7298
Dustin Milam <darklordsushi@yahoo.com>
2017-08-30 08:45:49 -0400

    Added config var for heroku staging

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -1,2 +1,2 @@
-APP_URL = 'https://app.easydash.io'
+APP_URL = ENV["HEROKU_ENV"] == 'producttion' ? 'https://app.easydash.io' : 'https://easydash-staging.herokuapp.com'
 DEV_URL = 'https://localhost:3000'
