4494813de64e20a4fa60ee282d170feb1f0d7e72
Dustin Milam <darklordsushi@yahoo.com>
2017-10-28 15:16:12 -0400

    added rake tasks for syncing store

---
+++ lib/functions/sync_store.rb
@@ -0,0 +1,17 @@
+class SyncStore
+
+	def self.single_store(shop)
+		session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
+		ShopifyAPI::Base.activate_session(session)
+		shop.getShopData
+		ShopifyAPI::Base.clear_session
+	end
+
+	def self.all_stores
+		shops = Shop.all
+		shops.each do |shop|
+			single_store(shop)
+		end
+	end
+
+end
--- lib/tasks/sync_store.rake
+++ lib/tasks/sync_store.rake
@@ -1,3 +1,7 @@
-task :sync => :environment do
-  Order_CSV.send_csv
+task :sync_single_store, [:shopify_domain] => :environment do |t, args|
+  SyncStore.single_store(Shop.find_by_shopify_domain(args[:shopify_domain]))
+end
+
+task :sync_all_stores => :environment do
+  SyncStore.all_stores
 end
